635d201e11d413554d4c1c6256aa7539e2bda9f1
GoodGuyzaxx <drawww111@gmail.com>
2023-10-30 13:48:07 +0900

    Update: Adding NavBar and chceking when login

--- app/src/main/java/me/zaxx/storyapp/view/main/MainActivity.kt
+++ app/src/main/java/me/zaxx/storyapp/view/main/MainActivity.kt
@@ -1,12 +1,46 @@
 package me.zaxx.storyapp.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.activity.viewModels
 import me.zaxx.storyapp.R
+import me.zaxx.storyapp.databinding.ActivityMainBinding
+import me.zaxx.storyapp.view.ViewModelFactory
+import me.zaxx.storyapp.view.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mainViewModel.getSession().observe(this){user ->
+            if (!user.isLogin){
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_item, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.btnLogout -> mainViewModel.logout()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/me/zaxx/storyapp/view/main/MainViewModel.kt
+++ app/src/main/java/me/zaxx/storyapp/view/main/MainViewModel.kt
@@ -1,6 +1,23 @@
 package me.zaxx.storyapp.view.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import me.zaxx.storyapp.data.pref.UserModel
+import me.zaxx.storyapp.data.repository.StoryRepository
+import java.util.concurrent.Flow
 
-class MainViewModel: ViewModel() {
+class MainViewModel(private val repository: StoryRepository): ViewModel() {
+
+    fun getSession(): LiveData<UserModel> {
+        return repository.getSession().asLiveData()
+    }
+
+    fun logout() {
+        viewModelScope.launch {
+            repository.logout()
+        }
+    }
 }
