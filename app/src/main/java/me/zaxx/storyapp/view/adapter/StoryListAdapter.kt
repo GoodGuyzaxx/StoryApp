39683f41ee792ed2edc693b5f402bc5177c47d27
GoodGuyzaxx <drawww111@gmail.com>
2023-10-30 13:49:13 +0900

    Update: adding Adapter

--- app/src/main/java/me/zaxx/storyapp/view/adapter/StoryListAdapter.kt
+++ app/src/main/java/me/zaxx/storyapp/view/adapter/StoryListAdapter.kt
@@ -1,6 +1,41 @@
 package me.zaxx.storyapp.view.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import me.zaxx.storyapp.data.retrofit.response.ListStoryItem
+import me.zaxx.storyapp.data.retrofit.response.StoryResponse
+import me.zaxx.storyapp.databinding.ItemStoryListBinding
 
-class StoryListAdapter {
+class StoryListAdapter:ListAdapter<ListStoryItem, StoryListAdapter.StoryViewHolder>(DIFF_CALLBACK) {
+    inner class StoryViewHolder(private val binding: ItemStoryListBinding): RecyclerView.ViewHolder(binding.root)  {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryViewHolder {
+        val binding = ItemStoryListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return StoryViewHolder(binding
+        )
+    }
+
+    override fun onBindViewHolder(holder: StoryViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    companion object{
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ListStoryItem>() {
+            override fun areItemsTheSame(oldItem: ListStoryItem, newItem: ListStoryItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ListStoryItem,
+                newItem: ListStoryItem
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+        }
+    }
 }
