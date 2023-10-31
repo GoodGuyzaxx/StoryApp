c7f5e0d559a7241c4375f524d0aa2ea68fa9171d
GoodGuyzaxx <drawww111@gmail.com>
2023-10-31 12:08:23 +0900

    Update: add ID to Data

--- app/src/main/java/me/zaxx/storyapp/data/pref/UserModel.kt
+++ app/src/main/java/me/zaxx/storyapp/data/pref/UserModel.kt
@@ -4,7 +4,8 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 data class UserModel(
-    val email: String,
+    val userId: String,
+    val name: String,
     val token: String,
     val isLogin: Boolean = false
 )
