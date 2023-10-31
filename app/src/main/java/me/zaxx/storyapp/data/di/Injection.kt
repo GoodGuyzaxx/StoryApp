19309ca62675865956113c8bb0abac3133d9a655
GoodGuyzaxx <drawww111@gmail.com>
2023-10-31 12:04:48 +0900

    Update: Change to Manual Token Header

--- app/src/main/java/me/zaxx/storyapp/data/di/Injection.kt
+++ app/src/main/java/me/zaxx/storyapp/data/di/Injection.kt
@@ -1,6 +1,8 @@
 package me.zaxx.storyapp.data.di
 
 import android.content.Context
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import me.zaxx.storyapp.data.pref.UserPreference
 import me.zaxx.storyapp.data.pref.dataStore
 import me.zaxx.storyapp.data.repository.StoryRepository
@@ -9,6 +11,7 @@ import me.zaxx.storyapp.data.retrofit.ApiConfig
 object Injection {
     fun provideRepository(context: Context): StoryRepository {
         val pref = UserPreference.getInstance(context.dataStore)
+//        val user = runBlocking { pref.getSession().first() }
         val apiService = ApiConfig.getApiService()
         return StoryRepository.getInstance(pref,apiService)
     }
