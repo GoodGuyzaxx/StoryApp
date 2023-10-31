78f97660a3d0c9ecc889397f31764de2e3a4b821
GoodGuyzaxx <drawww111@gmail.com>
2023-10-31 18:38:40 +0900

    Update: Adding Detail response Api Service

--- app/src/main/java/me/zaxx/storyapp/data/retrofit/ApiService.kt
+++ app/src/main/java/me/zaxx/storyapp/data/retrofit/ApiService.kt
@@ -1,5 +1,6 @@
 package me.zaxx.storyapp.data.retrofit
 
+import me.zaxx.storyapp.data.retrofit.response.DetailResponse
 import me.zaxx.storyapp.data.retrofit.response.ListStoryItem
 import me.zaxx.storyapp.data.retrofit.response.LoginResponse
 import me.zaxx.storyapp.data.retrofit.response.LoginResult
@@ -12,6 +13,7 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
 
@@ -34,4 +36,10 @@ interface ApiService {
     fun getStories(
         @Header("Authorization")token :String
     ): Call<StoryResponse>
+
+    @GET("stories/{id}")
+    fun getDetailStories(
+        @Header("Authorization")token: String,
+        @Path("id")id :String
+    ): Call<DetailResponse>
 }
