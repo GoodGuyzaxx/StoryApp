cf133d14842aa7a4e74daa77ea3891a42ecb6fe9
GoodGuyzaxx <drawww111@gmail.com>
2023-11-08 14:36:07 +0900

    update: Implement Paging

--- app/src/main/java/me/zaxx/storyapp/view/main/MainViewModel.kt
+++ app/src/main/java/me/zaxx/storyapp/view/main/MainViewModel.kt
@@ -4,15 +4,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import kotlinx.coroutines.launch
 import me.zaxx.storyapp.data.pref.UserModel
 import me.zaxx.storyapp.data.repository.StoryRepository
+import me.zaxx.storyapp.data.retrofit.response.ListStoryItem
 
 class MainViewModel(private val repository: StoryRepository): ViewModel() {
 
+ //    val listItem = repository.listItem
+ //    val listItem: LiveData<PagingData<ListStoryItem>> by lazy {
+ //        repository.getStories("Bearer ${repository.getToken()}").cachedIn(viewModelScope)
+ //    }
+    fun getStory(token: String) : LiveData<PagingData<ListStoryItem>> = repository.getStories(token).cachedIn(viewModelScope)
 
-    val listItem = repository.listItem
-    fun getStory(token: String) = repository.getStories(token)
 
     fun getSession(): LiveData<UserModel> {
         return repository.getSession().asLiveData()
