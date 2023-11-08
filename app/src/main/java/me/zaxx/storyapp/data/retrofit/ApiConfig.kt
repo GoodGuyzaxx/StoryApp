773c8144b183769430527a9fa3e14fa977e64812
GoodGuyzaxx <drawww111@gmail.com>
2023-11-08 14:32:12 +0900

    Update: Remove unused Code

--- app/src/main/java/me/zaxx/storyapp/data/retrofit/ApiConfig.kt
+++ app/src/main/java/me/zaxx/storyapp/data/retrofit/ApiConfig.kt
@@ -1,6 +1,5 @@
 package me.zaxx.storyapp.data.retrofit
 
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -9,16 +8,8 @@ object ApiConfig {
     fun getApiService(): ApiService {
         val loggingInterceptor =
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-//        val authInterceptor =Interceptor {chain ->
-//            val req = chain.request()
-//            val requestHeader = req.newBuilder()
-//                .addHeader("Authorization", "Bearer $token")
-//                .build()
-//            chain.proceed(requestHeader)
-//        }
         val client = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
-//            .addInterceptor(authInterceptor)
             .build()
         val retrofit = Retrofit.Builder()
             .baseUrl("https://story-api.dicoding.dev/v1/")
